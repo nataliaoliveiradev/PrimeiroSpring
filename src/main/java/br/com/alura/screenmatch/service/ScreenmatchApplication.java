6822a26e84f932ff722ab02cae419ec892e1f34e
nataliaoliveiradev <profissionalnataliaoliveira@gmail.com>
2025-01-20 22:29:37 -0300

    Adiciona a classe Principal.java

--- src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
+++ src/main/java/br/com/alura/screenmatch/service/ScreenmatchApplication.java
@@ -1,9 +1,7 @@
-package br.com.alura.screenmatch;
+package br.com.alura.screenmatch.service;
 
 import br.com.alura.screenmatch.model.DadosEpisodio;
 import br.com.alura.screenmatch.model.DadosTemporada;
-import br.com.alura.screenmatch.service.ConsumoApi;
-import br.com.alura.screenmatch.service.ConverteDados;
 import br.com.alura.screenmatch.model.DadosSerie;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
